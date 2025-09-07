7ebd68a1eb52d5e24e90f586c951e26557386800
Viktor <viktoryanenko11@gmail.com>
2025-09-07 14:39:31 +0300

    add get product by id

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProductsController } from "../controllers/products.js";
+import { getProductByIdController, getProductsController } from "../controllers/products.js";
 
 const router = Router();
 
 router.get('/products', getProductsController);
+router.get('/products/:productId', getProductByIdController);
 
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -4,3 +4,8 @@ export const getAllProducts = async () => {
     const products = await ProductModel.find();
     return products;
 };
+
+export const getProductById = async (productId) => {
+    const product = await ProductModel.findById(productId);
+    return product;
+};
